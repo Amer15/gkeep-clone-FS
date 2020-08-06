8ed104d9fbd2ee84c64318304d9aba0f419a3e4a
AMER <amer95521@gmail.com>
2020-08-06 16:01:28 +0530

    make it better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ else{
     app.use(morgan('combined'));
 }
 
-mongoose.connect( process.env.MONGODB_URI ,{
+mongoose.connect( process.env.MONGODB_URL ,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
